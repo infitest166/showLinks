bdbc2d7c192b1c84e660032a30f05e14779405fc
infi <infi@spboss.com>
2018-07-02 15:34:16 +0800

    add ver

---
+++ getVersion.php
@@ -0,0 +1,14 @@
+<?php
+  
+$data = array(
+  'tid' => 100, 
+  'app_version' => '标哥的技术博客',
+  'url' => 'itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/infitest166/showLinks/master/manifest.plist');
+   
+$response = array(
+  'code'  => 200, 
+  'message' => 'success for request',
+  'data'  => $data,
+  );
+  
+echojson_encode($response);
